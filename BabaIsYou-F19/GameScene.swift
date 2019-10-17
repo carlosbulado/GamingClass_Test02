f82461f1b6bc01354f3675023b7d22ef3dc823fd
Carlos José Bulado <carlosbulado@gmail.com>
2019-10-17 10:56:13 -0400

    R1: Baba can move and push the right objects

--- BabaIsYou-F19/GameScene.swift
+++ BabaIsYou-F19/GameScene.swift
@@ -9,10 +9,15 @@
 import SpriteKit
 import GameplayKit
 
-class GameScene: SKScene, SKPhysicsContactDelegate {
+class GameScene: SKScene, SKPhysicsContactDelegate
+{
+    var player : SKSpriteNode!
+    let PLAYER_SPEED : CGFloat = 30
 
-    override func didMove(to view: SKView) {
+    override func didMove(to view: SKView)
+    {
         self.physicsWorld.contactDelegate = self
+        self.player = self.childNode(withName: "coelho") as! SKSpriteNode
     }
    
     func didBegin(_ contact: SKPhysicsContact) {
@@ -23,7 +28,17 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         // Called before each frame is rendered
     }
     
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?)
+    {
+        let mouseTouch = touches.first
+        if (mouseTouch == nil) { return }
+        let location = mouseTouch!.location(in: self)
+        let nodeTouched = atPoint(location).name
+        
+        if (nodeTouched == "moveUp") { self.player.position.y = self.player.position.y + PLAYER_SPEED }
+        else if (nodeTouched == "moveDown") { self.player.position.y = self.player.position.y - PLAYER_SPEED }
+        else if (nodeTouched == "moveLeft") { self.player.position.x = self.player.position.x - PLAYER_SPEED }
+        else if (nodeTouched == "moveRight") { self.player.position.x = self.player.position.x + PLAYER_SPEED }
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
